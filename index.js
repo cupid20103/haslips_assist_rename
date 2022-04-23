ece741e12e1476234c4a5fd7406d6e2fa3d79bed
Piranha20103 <webphantom20103@gmail.com>
2022-04-23 22:19:19 +0900

    fix: update image data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ fs.readdir(folder, (err, files) => {
   for (const file of files) {
     var filePath = folder + file;
     var filePathRename =
-      folder + file.substring(0, file.length - 4) + "#1.25.png";
+      folder + file.substring(0, file.length - 4) + "#25.png";
 
     fs.rename(filePath, filePathRename, function (err) {
       if (err) console.log("ERROR: " + err);
